4ef984c19c7b5ada24536c3fab365cbd3b13ebad
ahhhnkitMVP~ <np03cs4a220458@heraldcollege.edu.np~>
2024-09-18 15:02:42 +0545

    01_first.c added in repo: C-program

--- 01_first.c
+++ 01_first.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-int add(int x, int y);
-void main() {
-    int a = 5, b = 5;
-    int sum = add(a + b);
-}
 
-int add(int x, int y){
-    printf("Sum=%d\n",x+y);
+int main() {
+    printf("Hello World!");
+    return 0;
 }
